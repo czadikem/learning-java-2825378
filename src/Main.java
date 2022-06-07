3d936d742bcf57fda293e2438b6288860b77434a
czadikem <35937408+czadikem@users.noreply.github.com>
2022-06-07 14:40:47 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
 
+    public static double salaryCalculator(double hoursPerWeek, double amountPerHour) {
+        double weeklyPaycheck = hoursPerWeek * amountPerHour;
+        return weeklyPaycheck * 52;
+    }
+
     public static void main(String[] args) {
-        
+        double salary = salaryCalculator(40, 15);
+        System.out.println(salary);
     }
 
 }
